4a58ac88a29d08b654890437cd86f7e12d88269d
Vikas Mohan <vikasmohan202@gmail.com>
2023-02-10 22:44:16 +0530

    Frist assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
+//task 1
+class A{
+
+    String  meth(){
+        return "Invoking method from class A";
+    }
+}
+// task 2
+class B extends A{
+
+}
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+        B b=new B();
+        b.meth();
+        B b1=new B();
+        b1.meth();
+    }
 }
